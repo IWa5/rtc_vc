fcbefc439241db38eeff786b26650b7968b19a8c
yikes.. <yikess2468@outlook.com>
2025-05-11 21:06:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: "*" }
 
-  app.get("/ping", (req, res) => {
+app.get("/ping", (req, res) => {
   res.status(200).send("pong");
 });
 
